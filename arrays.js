b73996947abfdbd60195ccaf0ec68146e0a38b40
kosatun@aol.com <kosatun@aol.com>
2018-06-30 03:19:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kikat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(toAdd);
+  array.unshift(element);
+  const array = 
 }
